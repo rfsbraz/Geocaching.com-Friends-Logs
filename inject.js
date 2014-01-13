5e566aa78336fcaa104076ea41f3ad36a1419f31
Rodrigo Braz <rfsbraz@gmail.com>
2014-01-13 00:09:20 +0000

    Added headers to the injected sections.

--- inject.js
+++ inject.js
@@ -5,11 +5,20 @@ function loadLogbookPage(pageIdx, showPersonal, showFriends) {
     $.getJSON("/seek/geocache.logbook", { tkn: userToken, idx: pageIdx + 1, num: 10, sp: showPersonal, sf: showFriends, decrypt: decryptLogs },
     function (response) {
         if (response.status == "success") {
-
+            
             if (response.pageInfo.rows > 0) {
-                var $newBody = $(document.createElement("TBODYF")); //If we use tbody, the page will load the next content page right after this log
-                $.tmpl("tmplCacheLogRow", response.data).appendTo($newBody);
-                $newBody.find("a.tb_images").each(function () {
+
+                var logTbody = $("#cache_logs_table")[0].firstChild;
+                var logContainer = $("#cache_logs_container")[0];
+                if(showPersonal){
+                    $("<h3>Friends Logs</h3>").insertBefore(logContainer.firstChild);
+                } else if (showFriends){
+                    $("<h3>My Log</h3>").insertBefore(logContainer.firstChild);
+                }
+
+                var logs = $.tmpl("tmplCacheLogRow", response.data);
+                logs.insertBefore(logTbody);
+                logs.find("a.tb_images").each(function () {
                     var $this = $(this);
                     $this.fancybox({
                         'type': 'image',
@@ -18,7 +27,8 @@ function loadLogbookPage(pageIdx, showPersonal, showFriends) {
                         titleFormat: function () { return $this.data('title'); }
                     });
                 });
-                $newBody.insertBefore($("#cache_logs_table")[0].firstChild);
+
+                $("<h3></br>Logbook</h3>").insertBefore($("tbody"));
             }
 
             isBusy = false;
